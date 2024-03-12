001f27d5d74cc7ca0b308e8c4355d67808e19115
Tuija Leinonen <77240343+tuijalei@users.noreply.github.com>
2024-03-12 10:00:04 +0200

    Update five_fold_cv.py

--- evaluation/five_fold_cv.py
+++ evaluation/five_fold_cv.py
@@ -11,9 +11,7 @@ from iterstrat.ml_stratifiers import MultilabelStratifiedShuffleSplit, Multilabe
 from utils import default_models
 from utils.utils import save_models
 from team_code import train_dx_model_team, train_digitization_model_team, \
-                        run_dx_model, run_digitization_model, \
-                        load_dx_model, load_digitization_model
-
+                        run_dx_model, run_digitization_model
 
 from helper_code import *
 
@@ -139,8 +137,6 @@ def run(data_folder,
             else:
                 raise
 
-        predictions.append(dx)
-        
         # Save Challenge outputs.
         output_path = os.path.split(output_record)[0]
         os.makedirs(output_path, exist_ok=True)
@@ -162,7 +158,142 @@ def run(data_folder,
     if verbose:
         print('Done.')
 
-    return predictions
+def evaluate(label_folder, records, output_folder, extra_scores = True):
+    
+    # Compute the signal reconstruction metrics.
+    channels = list()
+    records_completed_signal_reconstruction = list()
+    snrs = list()
+    snrs_median = list()
+    ks_metric = list()
+    asci_metric = list()
+    weighted_absolute_difference_metric = list()
+
+    # Iterate over the records.
+    for record in records:
+        # Load the signals, if available.
+        label_record = os.path.join(label_folder, record)
+        label_signal, label_fields = load_signal(label_record)
+
+        if label_signal is not None:
+            label_channels = label_fields['sig_name']
+            label_sampling_frequency = label_fields['fs']
+            label_num_samples = label_fields['sig_len']
+            channels.append(label_channels) # Use this variable if computing aggregate statistics for each channel.
+
+            output_record = os.path.join(output_folder, record)
+            output_signal, output_fields = load_signal(output_record)
+
+            if output_signal is not None:
+                output_channels = output_fields['sig_name']
+                output_sampling_frequency = output_fields['fs']                
+                output_num_samples = output_fields['sig_len']
+                records_completed_signal_reconstruction.append(record)
+
+                ###
+                ### TO-DO: Perform checks, such as sampling frequency, units, etc.
+                ###
+
+                # Reorder and trim or pad the signal as needed.
+                output_signal = reorder_signal(output_signal, output_channels, label_channels)
+                output_signal = trim_signal(output_signal, label_num_samples)
+
+            else:
+                output_signal = np.zeros(np.shape(label_signal), dtype=label_signal.dtype)
+
+            # Compute the signal reconstruction metrics.
+            channels = label_channels
+            sampling_frequency = label_sampling_frequency
+            num_channels = len(label_channels)
+
+            values = list()
+            for j in range(num_channels):
+                value = compute_snr(label_signal[:, j], output_signal[:, j])
+                values.append(value)
+            snrs.append(values)
+
+            if extra_scores:
+                values = list()
+                for j in range(num_channels):
+                    value = compute_snr_median(label_signal[:, j], output_signal[:, j])
+                    values.append(value)
+                snrs_median.append(values) 
+
+                values = list()
+                for j in range(num_channels):
+                    value = compute_ks_metric(label_signal[:, j], output_signal[:, j])
+                    values.append(value)
+                ks_metric.append(values)
+
+                values = list()
+                for j in range(num_channels):
+                    value = compute_asci_metric(label_signal[:, j], output_signal[:, j])
+                    values.append(value)
+                asci_metric.append(values)             
+    
+                values = list()
+                for j in range(num_channels):
+                    value = compute_weighted_absolute_difference(label_signal[:, j], output_signal[:, j], sampling_frequency)
+                    values.append(value)
+                weighted_absolute_difference_metric.append(values)
+
+    if records_completed_signal_reconstruction:
+        snrs = np.concatenate(snrs)
+        mean_snr = np.nanmean(snrs)
+
+        if extra_scores:
+            snrs_median = np.concatenate(snrs_median)
+            mean_snr_median = np.nanmean(snrs_median)
+
+            ks_metric = np.concatenate(ks_metric)
+            mean_ks_metric = np.nanmean(ks_metric)
+
+            asci_metric = np.concatenate(asci_metric)
+            mean_asci_metric = np.nanmean(asci_metric)
+
+            weighted_absolute_difference_metric = np.concatenate(weighted_absolute_difference_metric)
+            mean_weighted_absolute_difference_metric = np.nanmean(weighted_absolute_difference_metric)
+        else:
+            mean_snr_median = float('nan')
+            mean_ks_metric = float('nan')
+            mean_asci_metric = float('nan')     
+            mean_weighted_absolute_difference_metric = float('nan')          
+
+    else:
+        mean_snr = float('nan')
+        mean_snr_median = float('nan')
+        mean_ks_metric = float('nan')
+        mean_asci_metric = float('nan')  
+        mean_weighted_absolute_difference_metric = float('nan')          
+
+    # Compute the classification metrics.
+    records_completed_classification = list()
+    label_dxs = list()
+    output_dxs = list()
+
+    # Iterate over the records.
+    for record in records:
+        # Load the classes, if available.
+        label_record = os.path.join(label_folder, record)
+        label_dx = load_dx(label_record)
+
+        if label_dx:
+            output_record = os.path.join(output_folder, record)
+            output_dx = load_dx(output_record)
+
+            if output_dx:
+                records_completed_classification.append(record)
+
+            label_dxs.append(label_dx)
+            output_dxs.append(output_dx)
+
+    if records_completed_classification:
+        f_measure, _, _ = compute_f_measure(label_dxs, output_dxs)
+    else:
+        f_measure = float('nan')
+
+    # Return the results.
+    return mean_snr, mean_snr_median, mean_ks_metric, mean_asci_metric, mean_weighted_absolute_difference_metric, f_measure
 
 def five_fold_cv(data_folder=None, multilabel_cv=True, n_splits=5, train_models=True):
 
@@ -195,16 +326,33 @@ def five_fold_cv(data_folder=None, multilabel_cv=True, n_splits=5, train_models=
     # Iterate over CV splits
     for i, (train_idx, test_idx) in enumerate(kfold.split(indeces, labels)):
         train_records, test_records = list(map(records.__getitem__, train_idx)), list(map(records.__getitem__, test_idx))
-        
+        result_str += f'========== SPLIT {i+1} ========== \n'
+
+        # Initialize the directories where to store the models and the outputs
         model_folder = os.path.join('cv_results', 'trained models', f'split_{i+1}')
         os.makedirs(model_folder, exist_ok=True)
         
         output_folder = os.path.join('cv_results','output_folder', f'split_{i+1}')
         os.makedirs(output_folder, exist_ok=True)
 
+        # First, train digitalization model and classifier and then run them
         team_digit_model = train_digit_model(data_folder, train_records, model_folder, True)
         team_dx_model = train_dx_model(data_folder, train_records, model_folder, True)
-        preds = run(data_folder, test_records, output_folder, team_digit_model, team_dx_model,
-                    allow_failures=False, verbose=True)
+        run(data_folder, test_records, output_folder, team_digit_model, team_dx_model,
+            allow_failures=False, verbose=True)
 
-        # TBA: EVALUATION
+        # Evaluate the outputs
+        scores = evaluate(data_folder, test_records, output_folder)
+        snr, snr_median, ks_metric, asci_metric, mean_weighted_absolute_difference_metric, f_measure = scores
+        result_str += \
+            f'SNR: {snr:.3f}\n' + \
+            f'SNR median: {snr_median:.3f}\n' \
+            f'KS metric: {ks_metric:.3f}\n' + \
+            f'ASCI metric: {asci_metric:.3f}\n' \
+            f'Weighted absolute difference metric: {mean_weighted_absolute_difference_metric:.3f}\n' \
+            f'F-measure: {f_measure:.3f}\n\n'
+    
+    # Store the results
+    save_path = os.path.join('cv_results', 'results_' + datetime.now().strftime("%Y%m%d-%H%M") + '.txt')
+    with open(save_path, 'w') as f:
+         f.write(result_str)
