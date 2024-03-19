b3e71abcda4924bf5d278bd34eeb7e2ca1fecd27
sara-es <summerton.sara@gmail.com>
2024-03-19 14:55:39 +0000

    split CV scripts for digitization and classification
    Unfinished for now

--- evaluation/val_digitization.py
+++ evaluation/val_digitization.py
@@ -3,53 +3,23 @@ sys.path.append(os.path.join(sys.path[0], '..'))
 
 import numpy as np
 from datetime import datetime
+import argparse
 
-from sklearn.model_selection import train_test_split, KFold, StratifiedKFold
+from sklearn.model_selection import StratifiedKFold
 from sklearn.preprocessing import OneHotEncoder
-from iterstrat.ml_stratifiers import MultilabelStratifiedShuffleSplit, MultilabelStratifiedKFold # For multilabel stratification
+from iterstrat.ml_stratifiers import MultilabelStratifiedKFold # For multilabel stratification
 
-from utils import default_models
-from utils.utils import save_models
-from team_code import train_dx_model_team, train_digitization_model_team, \
-                        run_dx_model, run_digitization_model
+from utils import default_models, utils
+import team_code
 
-from helper_code import *
+import helper_code
 
-def train_dx_model(data_folder, 
-            record_ids, 
-            model_folder, 
-            verbose, 
-            models_to_train=default_models.DX_MODELS):
-    """
-    Parameters:
-        data_folder (str): The path to the foldder containing the data.
-        record_ids (list): The list of record ids, e.g. ['00001_lr', ...]
-        model_folder (str): The path to the folder where the models will be saved.
-        verbose (bool): Printouts?
-        models_to_train (list, default: "all"): A list of the models to train, used mainly for 
-            modular testing. Allows the user to specify which models should be trained. Default 
-            behaviour is to train all models listed in default_models. 
 
-    """
-    if verbose:
-        print('Training the dx classification model...')
+def generate_images(signal_folder, output_folder, verbose=True):
+    raise NotImplementedError
 
-    # Main function call. Pass in names of records here for cross-validation.
-    # DO NOT replace the argument to models_to_train with your model. 
-    # Add it to the default_models.py file instead.
-    models = train_dx_model_team(data_folder, record_ids, verbose, 
-                                 models_to_train=models_to_train)
 
-    # Save the model.
-    save_models(models, model_folder, verbose)
-    
-    if verbose:
-        print('Done.')
-        print()
-
-    return models
-
-def train_digit_model(data_folder, 
+def train_digit_model(images_folder, 
             record_ids, 
             model_folder, 
             verbose, 
@@ -70,7 +40,7 @@ def train_digit_model(data_folder,
         print('Training the digitization model...')
 
     # Main function call. Pass in names of records here for cross-validation.
-    models = train_digitization_model_team(data_folder, record_ids, verbose,
+    models = train_digitization_model_team(images_folder, record_ids, verbose,
                                            models_to_train=models_to_train)
 
     # Save the model.
@@ -81,6 +51,7 @@ def train_digit_model(data_folder,
         print()
 
     return models
+
 
 def run(data_folder, 
         records,
@@ -104,8 +75,6 @@ def run(data_folder,
     if verbose:
         print('Running the Challenge model(s) on the Challenge data...')
 
-    predictions = []
-
     # Iterate over the records.
     for i in range(num_records):
         if verbose:
@@ -117,23 +86,12 @@ def run(data_folder,
 
         # Run the digitization model. Allow or disallow the model to fail on some of the data, which can be helpful for debugging.
         try:
-            signal = run_digitization_model(digit_model, data_record, verbose) ### Teams: Implement this function!!!
-        except:
+            signal = team_code.run_digitization_model(digit_model, data_record, verbose)
+        except Exception as e:
             if allow_failures:
                 if verbose:
-                    print('... digitization failed.')
+                    print(f'... digitization failed on {records[i]} with {e}.')
                 signal = None
-            else:
-                raise
-
-        # Run the dx classification model. Allow or disallow the model to fail on some of the data, which can be helpful for debugging.
-        try:
-            dx = run_dx_model(dx_model, data_record, signal, verbose) ### Teams: Implement this function!!!
-        except:
-            if allow_failures:
-                if verbose >= 2:
-                    print('... dx classification failed.')
-                dx = None
             else:
                 raise
 
@@ -141,25 +99,22 @@ def run(data_folder,
         output_path = os.path.split(output_record)[0]
         os.makedirs(output_path, exist_ok=True)
 
-        data_header = load_header(data_record)
-        save_header(output_record, data_header)
+        data_header = helper_code.load_header(data_record)
+        helper_code.save_header(output_record, data_header)
 
         if signal is not None:
-            save_signal(output_record, signal)
+            helper_code.save_signal(output_record, signal)
 
             comment_lines = [l for l in data_header.split('\n') if l.startswith('#')]
-            signal_header = load_header(output_record)
+            signal_header = helper_code.load_header(output_record)
             signal_header += ''.join(comment_lines) + '\n'
-            save_header(output_record, signal_header)
-
-        if dx is not None:
-            save_dx(output_record, dx)
+            helper_code.save_header(output_record, signal_header)
 
     if verbose:
         print('Done.')
+        
 
 def evaluate(label_folder, records, output_folder, extra_scores = True):
-    
     # Compute the signal reconstruction metrics.
     channels = list()
     records_completed_signal_reconstruction = list()
@@ -173,7 +128,7 @@ def evaluate(label_folder, records, output_folder, extra_scores = True):
     for record in records:
         # Load the signals, if available.
         label_record = os.path.join(label_folder, record)
-        label_signal, label_fields = load_signal(label_record)
+        label_signal, label_fields = helper_code.load_signal(label_record)
 
         if label_signal is not None:
             label_channels = label_fields['sig_name']
@@ -182,7 +137,7 @@ def evaluate(label_folder, records, output_folder, extra_scores = True):
             channels.append(label_channels) # Use this variable if computing aggregate statistics for each channel.
 
             output_record = os.path.join(output_folder, record)
-            output_signal, output_fields = load_signal(output_record)
+            output_signal, output_fields = helper_code.load_signal(output_record)
 
             if output_signal is not None:
                 output_channels = output_fields['sig_name']
@@ -195,8 +150,8 @@ def evaluate(label_folder, records, output_folder, extra_scores = True):
                 ###
 
                 # Reorder and trim or pad the signal as needed.
-                output_signal = reorder_signal(output_signal, output_channels, label_channels)
-                output_signal = trim_signal(output_signal, label_num_samples)
+                output_signal = helper_code.reorder_signal(output_signal, output_channels, label_channels)
+                output_signal = helper_code.trim_signal(output_signal, label_num_samples)
 
             else:
                 output_signal = np.zeros(np.shape(label_signal), dtype=label_signal.dtype)
@@ -208,32 +163,32 @@ def evaluate(label_folder, records, output_folder, extra_scores = True):
 
             values = list()
             for j in range(num_channels):
-                value = compute_snr(label_signal[:, j], output_signal[:, j])
+                value = helper_code.compute_snr(label_signal[:, j], output_signal[:, j])
                 values.append(value)
             snrs.append(values)
 
             if extra_scores:
                 values = list()
                 for j in range(num_channels):
-                    value = compute_snr_median(label_signal[:, j], output_signal[:, j])
+                    value = helper_code.compute_snr_median(label_signal[:, j], output_signal[:, j])
                     values.append(value)
                 snrs_median.append(values) 
 
                 values = list()
                 for j in range(num_channels):
-                    value = compute_ks_metric(label_signal[:, j], output_signal[:, j])
+                    value = helper_code.compute_ks_metric(label_signal[:, j], output_signal[:, j])
                     values.append(value)
                 ks_metric.append(values)
 
                 values = list()
                 for j in range(num_channels):
-                    value = compute_asci_metric(label_signal[:, j], output_signal[:, j])
+                    value = helper_code.compute_asci_metric(label_signal[:, j], output_signal[:, j])
                     values.append(value)
                 asci_metric.append(values)             
     
                 values = list()
                 for j in range(num_channels):
-                    value = compute_weighted_absolute_difference(label_signal[:, j], output_signal[:, j], sampling_frequency)
+                    value = helper_code.compute_weighted_absolute_difference(label_signal[:, j], output_signal[:, j], sampling_frequency)
                     values.append(value)
                 weighted_absolute_difference_metric.append(values)
 
@@ -275,11 +230,11 @@ def evaluate(label_folder, records, output_folder, extra_scores = True):
     for record in records:
         # Load the classes, if available.
         label_record = os.path.join(label_folder, record)
-        label_dx = load_dx(label_record)
+        label_dx = helper_code.load_dx(label_record)
 
         if label_dx:
             output_record = os.path.join(output_folder, record)
-            output_dx = load_dx(output_record)
+            output_dx = helper_code.load_dx(output_record)
 
             if output_dx:
                 records_completed_classification.append(record)
@@ -288,41 +243,36 @@ def evaluate(label_folder, records, output_folder, extra_scores = True):
             output_dxs.append(output_dx)
 
     if records_completed_classification:
-        f_measure, _, _ = compute_f_measure(label_dxs, output_dxs)
+        f_measure, _, _ = helper_code.compute_f_measure(label_dxs, output_dxs)
     else:
         f_measure = float('nan')
 
     # Return the results.
     return mean_snr, mean_snr_median, mean_ks_metric, mean_asci_metric, mean_weighted_absolute_difference_metric, f_measure
 
-def five_fold_cv(data_folder=None, multilabel_cv=True, n_splits=5, train_models=True):
 
-    if data_folder is None:
-        data_folder = os.path.join('tiny_testset', 'records100')
+def five_fold_cv(data_folder=None, images_folder=None, generate_images=False, verbose=True):
+    result_str = datetime.now().strftime("%m/%d/%Y") + f" Test digitalization models: {default_models.DIGITIZATION_MODELS}\n"
 
-    # Find the records
-    records = find_records(data_folder)
-    if len(records) == 0:
-        raise FileNotFoundError('No data was provided.')
+    if generate_images:
+        if data_folder is None:
+            try:
+                data_folder = os.path.join('..', 'ptb-xl', 'records500', '00000')
+            except FileNotFoundError:
+                raise FileNotFoundError('No data was provided. Please provide a path to the data folder.')        
+        # pass in results string so we can print out parameters used to generate images
+        images_folder, result_str = generate_images(data_folder, images_folder, result_str, verbose=True)
 
-    # Find the labels
-    labels = []
-    for r in records:
-        currect_dx = load_dx(os.path.join(data_folder, r))
-        labels.append(currect_dx)
+    elif images_folder is None:
+        try:
+            images_folder = os.path.join('..', 'ptb-xl', 'img_records500', '00000')
+        except FileNotFoundError:
+            raise FileNotFoundError("No data was provided. Please provide a path to the folder containing generated "+\
+                                    "images, or use argument --generate_images True.")
+    else: # not generating images, images_folder is provided
+        print(f"Looking for images in {images_folder}...")   
 
-    if not labels:
-        raise Exception('There are no labels for the data.')
     
-    ohe = OneHotEncoder(sparse_output=False)
-    labels = ohe.fit_transform(labels)
-    uniq_labels = ohe.categories_[0]
-
-    # Initialize either multilabel stratified K-fold or standard stratified K-fold
-    kfold = MultilabelStratifiedKFold(n_splits = n_splits) if multilabel_cv else StratifiedKFold(n_splits=n_splits)
-    indeces = np.arange(len(records))
-
-    result_str = datetime.now().strftime("%m/%d/%Y") + f" Test digitalization models: {default_models.DIGITIZATION_MODELS} \t Test dx models: {default_models.DX_MODELS} \n"
     # Iterate over CV splits
     for i, (train_idx, test_idx) in enumerate(kfold.split(indeces, labels)):
         train_records, test_records = list(map(records.__getitem__, train_idx)), list(map(records.__getitem__, test_idx))
@@ -337,8 +287,8 @@ def five_fold_cv(data_folder=None, multilabel_cv=True, n_splits=5, train_models=
 
         # First, train digitalization model and classifier and then run them
         team_digit_model = train_digit_model(data_folder, train_records, model_folder, True)
-        team_dx_model = train_dx_model(data_folder, train_records, model_folder, True)
-        run(data_folder, test_records, output_folder, team_digit_model, team_dx_model,
+
+        run(data_folder, test_records, output_folder, team_digit_model,
             allow_failures=False, verbose=True)
 
         # Evaluate the outputs
@@ -356,3 +306,16 @@ def five_fold_cv(data_folder=None, multilabel_cv=True, n_splits=5, train_models=
     save_path = os.path.join('cv_results', 'results_' + datetime.now().strftime("%Y%m%d-%H%M") + '.txt')
     with open(save_path, 'w') as f:
          f.write(result_str)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--data_folder', type=str, required=True)
+    parser.add_argument('-i', '--images_folder', type=str, required=False)
+    parser.add_argument('-g', '--generate_images', type=bool, default=False)
+    parser.add_argument('-v', '--verbose', type=bool, default=True)
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = parse_args()
+    five_fold_cv(args)
