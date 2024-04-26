e5701e965d0d4bc1920bad476faa893115a13730
Dave Wong <davcom2+github@gmail.com>
2024-04-26 23:13:20 +0100

    Update pulse_detect_test.py
    - turn tolerance magic numbers into a variable
    - set stronger constraint to ensure up-pulse matches down-pulse in square wave

--- tests/pulse_detect_test.py
+++ tests/pulse_detect_test.py
@@ -20,8 +20,9 @@ import matplotlib.pyplot as plt
 
 # generate synthetic signal for debugging
 x = np.zeros(500)
-x[50:60] = 30
-#plt.plot(x)
+#x[50:60] = 30 # this should not return a pulse
+x[50:80] = 30 # this sohuld return a pulse
+
 
 
 # 1. diff the signals
@@ -33,24 +34,32 @@ plt.plot(xdiff)
 #left loop - is there a pulse on the left?
 idx = 0
 thresh = 25
-up = -1
+tol = 3 # how much variation we will accept due to noise/poor reconstruction
+up = -1 # everything is in pixels at this stage, so we can use negative numbers safely
 down = -1
+up_y = -1
 while idx < 100:
     if up == -1:
         if xdiff[idx] > thresh:
             up = idx # we expect this to be at the very start of the signal (i.e. idx == 0), but might not be if the digitisation is slightly wrong
+            up_y = xdiff[idx]
     else:  # if an up pulse has been found, then we are either on a false up, a plateau, or about to hit a down pulse 
         if (idx-up) > 25: #min width of pulse we will accept
-            if xdiff[idx] < (-thresh):
+            if abs(xdiff[idx] + up_y) < tol: # can make this constraint tighter to ensure that the down matches the up
                 down = idx #success!
         else:
-            if abs(xdiff[idx]) > 3: # if we actually aren't on a plateau, then this is not a square wave
+            if abs(xdiff[idx]) > tol: # if we actually aren't on a plateau, then this is not a square wave
                 up = -1 # reset the squarewave
+                up_y = -1
     idx = idx + 1
                 
 #right loop - is there a pulse on the right?
 
 #middle loop - is there a pulse in the middle
+
+
+#repeat for each row to check for consistency
+
 
 #logic at the end - is there a loop anywhere?
 #if so, what are the start and end indices?
