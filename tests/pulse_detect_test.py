e2bfe2dcf3b3d08638fc8bb6ff492be57dc06867
Dave Wong <davcom2+github@gmail.com>
2024-05-01 23:33:02 +0100

    Update pulse_detect_test.py
    added left, middle, right pulse checking for a single row of ECG

--- tests/pulse_detect_test.py
+++ tests/pulse_detect_test.py
@@ -18,52 +18,60 @@ import matplotlib.pyplot as plt
 
 # for each digitized line (usually 4 rows)
 
-# generate synthetic signal for debugging
+# loop across left, middle and right of signal
+def find_pulse(signal, start_idx, tol = 3, min_height = 25, min_width = 25, max_width = 100):
+    idx = start_idx
+    up = -1
+    down = -1
+    up_y = -1
+    
+    while idx < (start_idx + max_width):
+        if up == -1:
+            if xdiff[idx] > min_height:
+                up = idx # we expect this to be at the very start of the signal (i.e. idx == 0), but might not be if the digitisation is slightly wrong
+                up_y = xdiff[idx]
+        else:  # if an up pulse has been found, then we are either on a false up, a plateau, or about to hit a down pulse 
+            if (idx-up) > min_width: #min width of pulse we will accept
+                if abs(xdiff[idx] + up_y) < tol: # can make this constraint tighter to ensure that the down matches the up
+                    down = idx #success!
+            else:
+                if abs(xdiff[idx]) > tol: # if we actually aren't on a plateau, then this is not a square wave
+                    up = -1 # reset the squarewave
+                    up_y = -1
+        idx = idx + 1
+    
+    return up, down
+    
+
+
+## ----  generate synthetic signal for debugging ----#
 x = np.zeros(500)
 #x[50:60] = 30 # this should not return a pulse
 x[50:80] = 30 # this sohuld return a pulse
 
 
-
-# 1. diff the signals
-xdiff = np.diff(x)
+# --- find square waves -------#
+xdiff = np.diff(x) # diff signal
 plt.plot(xdiff)
 
-# loop across left, middle and right of signal
+# check for square wave on left, middle and right of signal
+max_width = 100
+l_up, l_down = find_pulse(xdiff,0) #check left
+m_up, m_down = find_pulse(xdiff,len(xdiff)//2-max_width//2) #check middle
+r_up, r_down = find_pulse(xdiff,len(xdiff)-max_width)
 
-#left loop - is there a pulse on the left?
-idx = 0
-thresh = 25
-tol = 3 # how much variation we will accept due to noise/poor reconstruction
-up = -1 # everything is in pixels at this stage, so we can use negative numbers safely
-down = -1
-up_y = -1
-while idx < 100:
-    if up == -1:
-        if xdiff[idx] > thresh:
-            up = idx # we expect this to be at the very start of the signal (i.e. idx == 0), but might not be if the digitisation is slightly wrong
-            up_y = xdiff[idx]
-    else:  # if an up pulse has been found, then we are either on a false up, a plateau, or about to hit a down pulse 
-        if (idx-up) > 25: #min width of pulse we will accept
-            if abs(xdiff[idx] + up_y) < tol: # can make this constraint tighter to ensure that the down matches the up
-                down = idx #success!
-        else:
-            if abs(xdiff[idx]) > tol: # if we actually aren't on a plateau, then this is not a square wave
-                up = -1 # reset the squarewave
-                up_y = -1
-    idx = idx + 1
-                
-#right loop - is there a pulse on the right?
+# todo - add sanity checking here - square wave should only exist in one place
 
-#middle loop - is there a pulse in the middle
-
-
-#repeat for each row to check for consistency
-
-
-#logic at the end - is there a loop anywhere?
-#if so, what are the start and end indices?
+# return the non -1 indices
+up = max([l_up, m_up, r_up])
+down = max([l_down, m_down, r_down])
+baseline = x[up] # TODO - check that this is the right index
 #RETURN: the pixel value of the bottom of the pulse (to get 0mV), start of pulse, end of pulse
                 
 
+## ---- each row will return an up and down index. Check for consistency ----
+ups = [up, up, up, up]
+downs = [down, down, down, down]
 
+# consensus agreement - if at least two sets of indices match, then accept the pulse
+# pulse index might be slightly different (if rotation isn't perfect), so take average if needed
