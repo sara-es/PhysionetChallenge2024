daaec1c0a10eae89b08cc609d8aca4a4ebd1e737
Dave Wong <davcom2+github@gmail.com>
2024-06-13 13:45:58 +0100

    Added gridsize setter and getter functions
    added get_gridsize_from_header
    added save_gridsize

--- utils/team_helper_code.py
+++ utils/team_helper_code.py
@@ -126,3 +126,20 @@ def load_image_paths(record):
             images.append(image_file_path)
 
     return images
+
+# Get the gridsize from a header or a similar string.
+def get_gridsize_from_header(string):
+    gridsize, has_gridsize = helper_code.get_variables(string, '# Gridsize:')
+    if has_gridsize:
+        gridsize = float(gridsize)
+    else:
+        raise Exception('No labels available: are you trying to load the labels from the held-out data, or did you forget to prepare the data to include the labels?')
+    return gridsize
+
+# Save the gridsize into header for a record. Assumes that gridsize is a number
+def save_gridsize(record, gridsize):
+    header_file = helper_code.get_header_file(record)
+    header = helper_code.load_text(header_file)
+    header += '#Gridsize: ' + str(gridsize) + '\n'
+    helper_code.save_text(header_file, header)
+    return header
