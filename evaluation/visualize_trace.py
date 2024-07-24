69d5969ef27b7dba0e0c52766cd32e5192c3bb65
sara-es <summerton.sara@gmail.com>
2024-07-24 10:56:28 +0100

    option to only plot reconstructions with low snr

--- evaluation/visualize_trace.py
+++ evaluation/visualize_trace.py
@@ -97,7 +97,7 @@ def get_trace(image, raw_signals, rhythm_leads=[Lead.II]):
 
 
 def visualize_trace(test_images_dir, unet_outputs_dir, reconstructed_signal_dir,
-                    wfdb_records_dir, visualization_save_dir):
+                    wfdb_records_dir, visualization_save_dir, save_images):
     records = helper_code.find_records(wfdb_records_dir)
     ids = team_helper_code.check_dirs_for_ids(records, test_images_dir, 
                                               unet_outputs_dir, True)
@@ -125,15 +125,9 @@ def visualize_trace(test_images_dir, unet_outputs_dir, reconstructed_signal_dir,
         # print(image_ids[i])
         image_info = {}
         image_info["record"] = records[i]
-        # set up mosaic for original image, u-net output with trace, ground truth signal, 
-        # and reconstructed signal
-        mosaic = plt.figure(layout="tight", figsize=(18, 13))
-        axd = mosaic.subplot_mosaic([
-                                        ['original_image', 'ecg_plots'],
-                                        ['trace', 'ecg_plots']
-                                    ])
+
         # load image
-        # TODO: this assumes image needs no preprocessing, or preprocessing has already been done
+        # this assumes image needs no preprocessing, or preprocessing has already been done
         with Image.open(os.path.join(test_images_dir, image_ids[i] + '.png')) as img:
             axd['original_image'].axis('off')
             axd['original_image'].imshow(img, cmap='gray')
@@ -147,12 +141,6 @@ def visualize_trace(test_images_dir, unet_outputs_dir, reconstructed_signal_dir,
         header_txt = helper_code.load_header(record_path)
         reconstructed_signal, raw_signals, gridsize = team_code.reconstruct_signal(records[i], unet_image, 
                                 header_txt, reconstructed_signal_dir, save_signal=True)
-        trace = get_trace(unet_image, raw_signals)
-        
-        # plot trace of signal from u-net output
-        # axd['trace'].xaxis.set_visible(False)
-        # axd['trace'].yaxis.set_visible(False)
-        axd['trace'].imshow(trace.data, cmap='gray')
 
         # load ground truth signal
         label_signal, label_fields = helper_code.load_signals(
@@ -169,54 +157,68 @@ def visualize_trace(test_images_dir, unet_outputs_dir, reconstructed_signal_dir,
                                                     int(label_fields["sig_len"])/4, 
                                                     rhythm=['II'])
         
-        # load rotation angle info from json
-        config_file = os.path.join(test_images_dir, image_ids[i] + '.json')
-        with open(config_file, 'r') as f:
-            config = json.load(f)
-        true_gridsize = config['x_grid']
-        dc_pulse = config['dc_pulse']
-        
         # calculate reconstruction SNR
         mean_snr, mean_snr_median, mean_ks_metric, mean_asci_metric, \
             mean_weighted_absolute_difference_metric = eval_utils.single_signal_snr(output_signal,
                         output_fields, label_signal, label_fields, records[i], extra_scores=True)
         gridsize = f'{gridsize:.2f}'
 
-        labels = None
-        # optional: classify signal
-        # labels = team_code.classify_signals(records[i], reconstructed_signal_dir, resnet_model, 
-        #                                     dx_classes, verbose=True)
+        # load image generation info from json
+        config_file = os.path.join(test_images_dir, image_ids[i] + '.json')
+        with open(config_file, 'r') as f:
+            config = json.load(f)
+        true_gridsize = config['x_grid']
+        dc_pulse = config['dc_pulse']
         
-        description_string = f"""{records[i]} ({label_fields['fs']} Hz)
+        if save_images or mean_snr < 4:
+            # set up mosaic for original image, u-net output with trace, ground truth signal, 
+            # and reconstructed signal
+            mosaic = plt.figure(layout="tight", figsize=(18, 13))
+            axd = mosaic.subplot_mosaic([
+                                            ['original_image', 'ecg_plots'],
+                                            ['trace', 'ecg_plots']
+                                        ])
+            # plot trace of signal from u-net output
+            trace = get_trace(unet_image, raw_signals)
+            axd['trace'].xaxis.set_visible(False)
+            axd['trace'].yaxis.set_visible(False)
+            axd['trace'].imshow(trace.data, cmap='gray')
+
+            labels = None
+            # optional: classify signal
+            # labels = team_code.classify_signals(records[i], reconstructed_signal_dir, resnet_model, 
+            #                                     dx_classes, verbose=True)
+            
+            description_string = f"""{records[i]} ({label_fields['fs']} Hz)
     Reconstruction SNR: {mean_snr:.2f}
     Gridsize: {gridsize}
     {label_fields['comments'][1:-1]}
     Predicted labels: {labels}"""
-    
-        # plot ground truth and reconstructed signal
-        fig, axs = plt.subplots(reconstructed_signal.shape[1], 1, figsize=(9, 12.5), sharex=True)
-        fig.subplots_adjust(hspace=0.1)
-        fig.suptitle(description_string)
-        # fig.text(0.5, 0.95, description_string, ha='center')
-        for j in range(reconstructed_signal.shape[1]):
-            if j == 0:
-                axs[j].plot(label_signal[:, j], label='Ground Truth Signal')
-                axs[j].plot(reconstructed_signal[:, j], label='Reconstructed Signal', alpha=0.8)
-                axs[j].legend(loc='upper right')
-            else:
-                axs[j].plot(label_signal[:, j])
-                axs[j].plot(reconstructed_signal[:, j], alpha=0.8)
-            axs[j].set_ylabel(f'{label_fields["sig_name"][j]}', rotation='horizontal')
-            # axs[j].set_yrotation(0)
-        fig.canvas.draw()
-        axd['ecg_plots'].axis('off')
-        axd['ecg_plots'].imshow(fig.canvas.renderer.buffer_rgba())
+        
+            # plot ground truth and reconstructed signal
+            fig, axs = plt.subplots(reconstructed_signal.shape[1], 1, figsize=(9, 12.5), sharex=True)
+            fig.subplots_adjust(hspace=0.1)
+            fig.suptitle(description_string)
+            # fig.text(0.5, 0.95, description_string, ha='center')
+            for j in range(reconstructed_signal.shape[1]):
+                if j == 0:
+                    axs[j].plot(label_signal[:, j], label='Ground Truth Signal')
+                    axs[j].plot(reconstructed_signal[:, j], label='Reconstructed Signal', alpha=0.8)
+                    axs[j].legend(loc='upper right')
+                else:
+                    axs[j].plot(label_signal[:, j])
+                    axs[j].plot(reconstructed_signal[:, j], alpha=0.8)
+                axs[j].set_ylabel(f'{label_fields["sig_name"][j]}', rotation='horizontal')
+                # axs[j].set_yrotation(0)
+            fig.canvas.draw()
+            axd['ecg_plots'].axis('off')
+            axd['ecg_plots'].imshow(fig.canvas.renderer.buffer_rgba())
 
-        # save everything in one image
-        filename = f"{records[i]}_reconstruction.png"
-        plt.figure(mosaic)
-        plt.savefig(os.path.join(visualization_save_dir, filename))
-        plt.close()
+            # save everything in one image
+            filename = f"{records[i]}_reconstruction.png"
+            plt.figure(mosaic)
+            plt.savefig(os.path.join(visualization_save_dir, filename))
+            plt.close()
 
         image_info["snr"] = mean_snr
         image_info["snr_median"] = mean_snr_median
@@ -246,9 +248,11 @@ if __name__ == "__main__":
     os.makedirs(reconstructed_signal_dir, exist_ok=True)
     visualization_save_folder = os.path.join("evaluation", "trace_visualizations")
     os.makedirs(visualization_save_folder, exist_ok=True)
+    save_all_images = True # save all images (True), or only ones with poor snr (False)
 
     visualize_trace(test_images_folder, 
                     unet_outputs_folder, 
                     reconstructed_signal_dir,
                     wfdb_records_dir=test_images_folder,
-                    visualization_save_dir=visualization_save_folder)
+                    visualization_save_dir=visualization_save_folder,
+                    save_images=save_all_images)
