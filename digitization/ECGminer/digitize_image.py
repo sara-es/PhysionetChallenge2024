add9d577c078845459c2594abacd5c9e7dbad7ee
sara-es <summerton.sara@gmail.com>
2024-08-13 16:35:09 +0100

    use yolo outputs for row detection

--- digitization/ECGminer/digitize_image.py
+++ digitization/ECGminer/digitize_image.py
@@ -9,6 +9,25 @@ from digitization.ECGminer.Preprocessor import Preprocessor
 from digitization.ECGminer import extract_signals, vectorize_signals
 
 
+def get_ECG_rows(yolo_boxes):
+
+    yolo_boxes = np.array(yolo_boxes)
+    #get the number of long leads
+    long_leads = sum(yolo_boxes[:,0])
+    num_rows = 3 + long_leads #simplifying challenge assumption that the short leads are in 3x4 format.
+    # n.b. to generalise this, we need to work out the number of short lead rows - we could do this by merging rows in the x direction.
+    
+    # get total bounding box
+    left = min(yolo_boxes[:, 1] - yolo_boxes[:, 3]/2)
+    right = max(yolo_boxes[:, 1] + yolo_boxes[:, 3]/2)
+    top = min(yolo_boxes[:, 2] - yolo_boxes[:, 4]/2)
+    bottom = max(yolo_boxes[:, 2] - yolo_boxes[:, 4]/2)
+   
+    bounding_box = [left, right, top, bottom]
+    
+    return num_rows, bounding_box
+
+
 def digitize_image_unet(restored_image, yolo_rois, sig_len=1000, max_duration=10, is_generated_image=True):
     """ 
     digitize_image_unet takes the output of the u-net
@@ -29,6 +48,9 @@ def digitize_image_unet(restored_image, yolo_rois, sig_len=1000, max_duration=10
     restored_image = np.uint8(restored_image)
     restored_image = Image(restored_image)
 
+    # Convert YOLO xywh boxes to ROIs
+    num_rows, bounding_box = get_ECG_rows(yolo_rois)
+
     # returns image object
     preprocessor = Preprocessor()
     ecg_crop, rect = preprocessor.preprocess(restored_image)
@@ -36,8 +58,7 @@ def digitize_image_unet(restored_image, yolo_rois, sig_len=1000, max_duration=10
     ## DW: replace extract_signals with our own version.
     # returns x and y coordinates of the traces in order
     # raises DigitizationError if failure occurs.
-    # hardcoded n_lines=4 for now because constant layout+rhythm
-    signal_coords, rois = extract_signals.extract_row_signals(ecg_crop, n_lines=4)
+    signal_coords, rois = extract_signals.extract_row_signals(ecg_crop, n_lines=num_rows)
 
     # check for reference pulses, then convert to digitized signals
     # returns array of digitized signals, original signal coordinates, and gridsize
