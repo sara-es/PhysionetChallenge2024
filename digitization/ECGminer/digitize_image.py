54b5e987cd06b834fd30c9585f5e40efc5b15547
Dave Wong <davcom2+github@gmail.com>
2024-08-14 16:23:10 +0100

    integrate yolo bounding boxes for cropping ECG image
    - bug fixes to get_ECG_rows
    - use YOLO bounding box to crop ECG image (turned on or off from utils.constants

--- digitization/ECGminer/digitize_image.py
+++ digitization/ECGminer/digitize_image.py
@@ -3,10 +3,12 @@ Entry point to ecg-miner digitization code
 """
 import cv2
 import numpy as np
-import pandas as pd
+#import pandas as pd
 from digitization.ECGminer.assets.Image import Image
-from digitization.ECGminer.Preprocessor import Preprocessor
 from digitization.ECGminer import extract_signals, vectorize_signals
+from digitization.ECGminer.assets.Point import Point
+from digitization.ECGminer.assets.Rectangle import Rectangle
+from utils import constants
 
 
 def get_ECG_rows(yolo_boxes):
@@ -20,12 +22,12 @@ def get_ECG_rows(yolo_boxes):
     left = min(yolo_boxes[:, 1] - yolo_boxes[:, 3]/2)
     right = max(yolo_boxes[:, 1] + yolo_boxes[:, 3]/2)
     top = min(yolo_boxes[:, 2] - yolo_boxes[:, 4]/2)
-    bottom = max(yolo_boxes[:, 2] - yolo_boxes[:, 4]/2)
+    bottom = max(yolo_boxes[:, 2] + yolo_boxes[:, 4]/2)
    
     # note bounding box is in percentage of image size
     bounding_box = [left, right, top, bottom]
     
-    return num_rows, bounding_box
+    return int(num_rows), bounding_box
 
 
 def digitize_image_unet(restored_image, yolo_rois, sig_len=1000, max_duration=10, is_generated_image=True):
@@ -49,16 +51,36 @@ def digitize_image_unet(restored_image, yolo_rois, sig_len=1000, max_duration=10
     restored_image = Image(restored_image)
 
     # Convert YOLO xywh boxes to ROIs
-    num_rows, bounding_box = get_ECG_rows(yolo_rois)
+    num_rows, box = get_ECG_rows(yolo_rois)
+    
+    #replace preprocessor with our own cropper
+    height = restored_image.shape[0]
+    width = restored_image.shape[1]
 
-    # returns image object
-    preprocessor = Preprocessor()
-    ecg_crop, rect = preprocessor.preprocess(restored_image)
+    #convert [left, right, top, bottom] into pixels
+    pad = 20 #bounding box is pretty good, so let's add a just a small amount of padding
+    box[0] = int(np.round(box[0] * width)) - pad
+    box[1] = int(np.round(box[1] * width)) + pad
+    box[2] = int(np.round(box[2] * height)) - pad
+    box[3] = int(np.round(box[3] * height)) + pad
+    
+    # sanity check just in case bounding box was near edge of image
+    if box[0] < 0 : box[0] = 0
+    if box[1] > width : box[1] = width
+    if box[2] < 0 : box[2] = 0
+    if box[3] < height : box[3] = height
+
+    if constants.YOLO_BOUNDING:
+        rect = Rectangle(Point(box[0], box[2]), Point(box[1], box[3])) # cropped image from yolo
+    else:
+        rect = Rectangle(Point(0, 350), Point(width, height))
+    
+    restored_image.crop(rect)
     
     ## DW: replace extract_signals with our own version.
     # returns x and y coordinates of the traces in order
     # raises DigitizationError if failure occurs.
-    signal_coords, rois = extract_signals.extract_row_signals(ecg_crop, n_lines=int(num_rows))
+    signal_coords, rois = extract_signals.extract_row_signals(restored_image, n_lines=int(num_rows))
 
     # check for reference pulses, then convert to digitized signals
     # returns array of digitized signals, original signal coordinates, and gridsize
