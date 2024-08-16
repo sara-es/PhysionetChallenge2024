5622b1e33e49e1fc7ca6a4f5e73bed54b5d1df52
Dave Wong <davcom2+github@gmail.com>
2024-08-16 17:32:02 +0100

    update get_ecg_rows
    update to deal with possibility of YOLO incorrectly double counting a long lead

--- digitization/ECGminer/digitize_image.py
+++ digitization/ECGminer/digitize_image.py
@@ -11,22 +11,37 @@ from digitization.ECGminer.assets.Rectangle import Rectangle
 from utils import constants
 
 
-def get_ECG_rows(yolo_boxes):
+def get_ECG_rows(yolo_boxes, sens = 12):
     yolo_boxes = np.array(yolo_boxes)
-    #get the number of long leads
-    long_leads = sum(yolo_boxes[:,0])
-    num_rows = 3 + long_leads #simplifying challenge assumption that the short leads are in 3x4 format.
-    # n.b. to generalise this, we need to work out the number of short lead rows - we could do this by merging rows in the x direction.
     
     # get total bounding box
     left = min(yolo_boxes[:, 1] - yolo_boxes[:, 3]/2)
     right = max(yolo_boxes[:, 1] + yolo_boxes[:, 3]/2)
     top = min(yolo_boxes[:, 2] - yolo_boxes[:, 4]/2)
     bottom = max(yolo_boxes[:, 2] + yolo_boxes[:, 4]/2)
-   
+    
     # note bounding box is in percentage of image size
     bounding_box = [left, right, top, bottom]
     
+    # long_leads is subset of bounding boxes with long leads:
+    long_leads = yolo_boxes[yolo_boxes[:,0]==1]
+    vrange = bottom-top
+    thresh = vrange / sens #assume there are 6 rows maximum, and we want to find half of that distance as a threshold
+
+    #get the number of long leads
+    if long_leads.shape[0] > 1:
+        # merge any boxes that are on similar rows
+            long_roi = long_leads[:,2]
+            long_roi = np.sort(long_roi)
+            diffs = np.diff(long_roi)
+            diffs = diffs < thresh
+            num_long_leads = sum(diffs==False) + 1
+    else:
+        num_long_leads = sum(yolo_boxes[:,0])
+    
+    num_rows = 3 + num_long_leads #simplifying challenge assumption that the short leads are in 3x4 format.
+    # n.b. to generalise this, we need to work out the number of short lead rows - we could do this by merging rows in the x direction.
+   
     return int(num_rows), bounding_box
 
 
