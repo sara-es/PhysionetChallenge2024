70eeafb194fc9407b5a83d72a1f8969877772c36
David Wong <hssdwo@leeds.ac.uk>
2024-08-20 15:52:15 +0100

    Update preprocess.py
    create alternative to get_ECG_rows (get_ECG_rows_2)

--- digitization/ECGminer/preprocess.py
+++ digitization/ECGminer/preprocess.py
@@ -4,6 +4,7 @@ from digitization.ECGminer.assets.Image import Image
 from digitization.ECGminer.assets.Point import Point
 from digitization.ECGminer.assets.Rectangle import Rectangle
 from utils import constants
+from skimage.measure import label
 
 def get_ECG_rows(yolo_boxes, sens=12):
     yolo_boxes = np.array(yolo_boxes)
@@ -39,6 +40,64 @@ def get_ECG_rows(yolo_boxes, sens=12):
     
     num_rows = 3 + num_long_leads #simplifying challenge assumption that the short leads are in 3x4 format.
     # n.b. to generalise this, we need to work out the number of short lead rows - we could do this by merging rows in the x direction.
+    
+    return int(num_rows), bounding_box
+
+''' alternative method for getting number of ECG rows, assuming that the long-lead label is unreliable. Logic is:
+    1. Expand each yolo_box slightly
+    2. merge any overlapping yolo_boxes
+    3. count the number of remaining merged boxes
+    '''
+def get_ECG_rows_2(yolo_boxes):
+    yolo_boxes = np.array(yolo_boxes)
+    # n.b. to generalise this, we need to work out the number of short lead rows - we could do this by merging rows in the x direction.
+    
+    # get total bounding box
+    left = min(yolo_boxes[:, 1] - yolo_boxes[:, 3]/2)
+    right = max(yolo_boxes[:, 1] + yolo_boxes[:, 3]/2)
+    top = min(yolo_boxes[:, 2] - yolo_boxes[:, 4]/2)
+    bottom = max(yolo_boxes[:, 2] + yolo_boxes[:, 4]/2)
+   
+    # add buffer to top (~5% of height)
+    top = max(0, top - 0.05)
+
+    # note bounding box is in percentage of image size
+    bounding_box = [left, right, top, bottom]
+    
+    pad = 0.02
+    # expand each bounding box slightly
+    all_left = (yolo_boxes[:, 1] - yolo_boxes[:, 3]/2 - pad)
+    all_right = (yolo_boxes[:, 1] + yolo_boxes[:, 3]/2 + pad)
+    all_top = (yolo_boxes[:, 2] - yolo_boxes[:, 4]/2 + pad)
+    all_bottom = (yolo_boxes[:, 2] + yolo_boxes[:, 4]/2 - pad)
+    
+    all_left[all_left<0] = 0
+    all_right[all_right>1] = 1 
+    all_top[all_top<0] = 0
+    all_bottom[all_bottom>1] = 1 
+    
+    all_left = np.round(all_left*100,0)
+    all_right= np.round(all_right*100,0)
+    all_top = np.round(all_top*100,0)
+    all_bottom = np.round(all_bottom*100,0)
+    
+    all_left = all_left.astype(int)
+    all_right = all_right.astype(int)
+    all_top = all_top.astype(int)
+    all_bottom = all_bottom.astype(int)
+    
+    #create black 100x100 np array
+    mask = np.zeros([100,100])
+    for idx, val in enumerate(all_left):
+        if all_bottom[idx] - all_top[idx]  < 4:
+            all_top[idx] = all_top[idx] - 3
+            all_bottom[idx] = all_bottom[idx] + 3
+        mask[all_top[idx]:all_bottom[idx], all_left[idx]:all_right[idx]] = 1
+    
+    
+    #count number of contiguous sections in mask
+    mask = label(mask)
+    num_rows = np.max(mask)
     
     return int(num_rows), bounding_box
 
