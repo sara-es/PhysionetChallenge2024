95e6cad60f3658009a7a8d8c2d51f0a8f9ef41b2
sara-es <summerton.sara@gmail.com>
2024-08-21 13:00:43 +0100

    accommodate for single class roi detection

--- digitization/ECGminer/preprocess.py
+++ digitization/ECGminer/preprocess.py
@@ -8,7 +8,8 @@ from utils import constants
 def get_ECG_rows(yolo_boxes, sens=12):
     yolo_boxes = np.array(yolo_boxes)
     # n.b. to generalise this, we need to work out the number of short lead rows - we could do this by merging rows in the x direction.
-    
+    if yolo_boxes.size == 0: # no bounding boxes; guess 3 rows
+        return 3, [0, 1, 0, 1]
     # get total bounding box
     left = min(yolo_boxes[:, 1] - yolo_boxes[:, 3]/2)
     right = max(yolo_boxes[:, 1] + yolo_boxes[:, 3]/2)
@@ -21,24 +22,27 @@ def get_ECG_rows(yolo_boxes, sens=12):
     # note bounding box is in percentage of image size
     bounding_box = [left, right, top, bottom]
     
-    # long_leads is subset of bounding boxes with long leads:
-    long_leads = yolo_boxes[yolo_boxes[:,0]==1]
-    vrange = bottom-top
-    thresh = vrange / sens #assume there are 6 rows maximum, and we want to find half of that distance as a threshold
+    if max(yolo_boxes[:,0]) > 0: # using 2 classes or more yolo version
+        # long_leads is subset of bounding boxes with long leads:
+        long_leads = yolo_boxes[yolo_boxes[:,0]==1]
+        vrange = bottom-top
+        thresh = vrange / sens #assume there are 6 rows maximum, and we want to find half of that distance as a threshold
 
-    #get the number of long leads
-    if long_leads.shape[0] > 1:
-        # merge any boxes that are on similar rows
-            long_roi = long_leads[:,2]
-            long_roi = np.sort(long_roi)
-            diffs = np.diff(long_roi)
-            diffs = diffs < thresh
-            num_long_leads = sum(diffs==False) + 1
-    else:
-        num_long_leads = sum(yolo_boxes[:,0])
+        #get the number of long leads
+        if long_leads.shape[0] > 1:
+            # merge any boxes that are on similar rows
+                long_roi = long_leads[:,2]
+                long_roi = np.sort(long_roi)
+                diffs = np.diff(long_roi)
+                diffs = diffs < thresh
+                num_long_leads = sum(diffs==False) + 1
+        else:
+            num_long_leads = sum(yolo_boxes[:,0])
     
-    num_rows = 3 + num_long_leads #simplifying challenge assumption that the short leads are in 3x4 format.
-    # n.b. to generalise this, we need to work out the number of short lead rows - we could do this by merging rows in the x direction.
+        num_rows = 3 + num_long_leads #simplifying challenge assumption that the short leads are in 3x4 format.
+        # n.b. to generalise this, we need to work out the number of short lead rows - we could do this by merging rows in the x direction.
+    else: # using 1 class yolo version
+        num_rows = len(yolo_boxes)
     
     return int(num_rows), bounding_box
 
